d48b1c81c4d428a28841bd8f854b2f9c5863a6f9
HemantPundpal <47489403+HemantPundpal@users.noreply.github.com>
2020-01-30 14:33:58 +1300

    Introduction to ADO.NET

---
+++ StoredProceduresWithOutputParametersinCSharp/StoredProceduresWithOutputParametersinCSharp/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="StoredProceduresWithOutputParametersinCSharp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="LFirstName" runat="server" Text="First Name"></asp:Label>
+&nbsp;&nbsp;
+            <asp:TextBox ID="TbxFirstName" runat="server" Width="160px"></asp:TextBox>
+            <br />
+            <asp:Label ID="LLastName" runat="server" Text="Last Name"></asp:Label>
+&nbsp;&nbsp;
+            <asp:TextBox ID="TbxLastName" runat="server" Width="162px"></asp:TextBox>
+            <br />
+            <asp:Label ID="LEmail" runat="server" Text="Email"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TbxEmail" runat="server" Width="163px"></asp:TextBox>
+            <br />
+            <asp:Button ID="BtnSubmitStudent" runat="server" OnClick="BtnSubmitStudent_Click" Text="Submit Student" />
+            <br />
+            <asp:Label ID="LMessage" runat="server" Text="Message"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StoredProceduresWithOutputParametersinCSharp/StoredProceduresWithOutputParametersinCSharp/WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StoredProceduresWithOutputParametersinCSharp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSubmitStudent_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using(SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command1 = new SqlCommand("spAddStudent", connection);
+                command1.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command1.Parameters.AddWithValue("@FirstName", TbxFirstName.Text);
+                command1.Parameters.AddWithValue("@LastName", TbxLastName.Text);
+
+                SqlParameter outputParameter = new SqlParameter();
+                outputParameter.ParameterName = "@StudentId";
+                outputParameter.SqlDbType = System.Data.SqlDbType.Int;
+                outputParameter.Direction = System.Data.ParameterDirection.Output;
+                command1.Parameters.Add(outputParameter);
+
+                connection.Open();
+                command1.ExecuteNonQuery();
+
+                string StudentId = outputParameter.Value.ToString();
+
+                SqlCommand command2 = new SqlCommand($"INSERT INTO Emails VALUES ('{TbxEmail.Text}', {StudentId})", connection);
+                command2.ExecuteNonQuery();
+
+                LMessage.Text = "Student Id " + StudentId;
+            }
+
+        }
+    }
+}
