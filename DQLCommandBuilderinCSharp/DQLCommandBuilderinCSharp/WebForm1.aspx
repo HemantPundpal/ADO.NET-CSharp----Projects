1dab5b294e48cc410a5ed600ebe50c286840fb75
HemantPundpal <47489403+HemantPundpal@users.noreply.github.com>
2020-01-30 14:43:43 +1300

    Introduction to ADO.NET

---
+++ DQLCommandBuilderinCSharp/DQLCommandBuilderinCSharp/WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DQLCommandBuilderinCSharp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="LblStudentId" runat="server" Text="Enter Student ID"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TbxStudentId" runat="server"></asp:TextBox>
+        <asp:Button ID="BtnLoadStudent" runat="server" Text="Load Student" OnClick="BtnLoadStudent_Click" />
+        <asp:Label ID="LblMessage" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="LblStudentFirstName" runat="server" Text="Student First Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TbxStudentFirstName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="LblStudentLastName0" runat="server" Text="Student Last Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TbxStudentLastName" runat="server"></asp:TextBox>
+        <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" />
+        <br />
+        <br />
+        <asp:Label ID="LblInsert" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="LblUpdate" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="LblDelete" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ DQLCommandBuilderinCSharp/DQLCommandBuilderinCSharp/WebForm1.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DQLCommandBuilderinCSharp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLoadStudent_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection connection1 = new SqlConnection(ConnectionString))
+            {
+                string sqlQuery = "SELECT * FROM Students WHERE Id = @StudentId";
+                SqlDataAdapter dataAdapter1 = new SqlDataAdapter(sqlQuery, connection1);
+                dataAdapter1.SelectCommand.Parameters.AddWithValue("@StudentId", TbxStudentId.Text);
+
+                DataSet dataSet1 = new DataSet();
+                dataAdapter1.Fill(dataSet1, "Students");
+
+                ViewState["QUERY"] = sqlQuery;
+                ViewState["DATASET"] = dataSet1;
+
+                if(dataSet1.Tables["Students"].Rows.Count > 0)
+                {
+                    DataRow dr = dataSet1.Tables["Students"].Rows[0];
+                    TbxStudentFirstName.Text = dr["FirstName"].ToString();
+                    TbxStudentLastName.Text = dr["LastName"].ToString();
+                }
+                else
+                {
+                    LblMessage.Text = "No Student with ID - " + TbxStudentId.Text;
+                    TbxStudentFirstName.Text = string.Empty;
+                    TbxStudentLastName.Text = string.Empty;
+                }
+
+                //GrdvStudentsbyId.DataSource = dataSet3;
+                //GrdvStudentsbyId.DataBind();
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection connection1 = new SqlConnection(ConnectionString))
+            {
+                SqlDataAdapter dataAdapter1 = new SqlDataAdapter((string)ViewState["QUERY"], connection1);
+                dataAdapter1.SelectCommand.Parameters.AddWithValue("@StudentId", TbxStudentId.Text);
+                SqlCommandBuilder cmdBuilder1 = new SqlCommandBuilder(dataAdapter1); // This binding of dataAdapter with commandBuilder is must for auto dataset update queries to work
+                DataSet dataSet1 = (DataSet)ViewState["DATASET"];
+
+                if (dataSet1.Tables["Students"].Rows.Count > 0)
+                {
+                    DataRow dr = dataSet1.Tables["Students"].Rows[0];
+                    dr["FirstName"] = TbxStudentFirstName.Text;
+                    dr["LastName"] = TbxStudentLastName.Text;
+                }
+
+                int rowsUpdated = dataAdapter1.Update(dataSet1, "Students");
+
+                if (rowsUpdated > 0)
+                {
+                    LblMessage.Text = $"Student table - {rowsUpdated} row(s) updated.";
+                }
+                else
+                {
+                    LblMessage.Text = $"Student table - no row(s) updated.";
+                }
+
+                dataAdapter1.SelectCommand.Parameters.AddWithValue("@StudentId", TbxStudentId.Text);
+            }
+        }
+    }
+}
